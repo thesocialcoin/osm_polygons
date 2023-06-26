0e11c95e40178b74ef41b04b4f2d5beaebe534ec
Óscar Delgado <odelgado@citibeats.com>
2023-06-26 16:57:43 +0200

    First attempt of library functions

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 //index.js
 
+import cities from './geojson/cities.json'
 import countries from './geojson/countries.json'
+import regions from './geojson/regions.json'
+import subregions from './geojson/subregions.json'
 
-function testFunction() {
-    console.log(Object.keys(countries))
+function getCityPolygon(code) {
+    try {
+        return cities[code]
+    } catch {
+        return {}
+    }
 }
 
-export default testFunction
+function getCountryPolygon(code) {
+    try {
+        return countries[code]
+    } catch {
+        return {}
+    }
+}
+
+function getRegionPolygon(code) {
+    try {
+        return regions[code]
+    } catch {
+        return {}
+    }
+}
+
+function getSubregionPolygon(code) {
+    try {
+        return subregions[code]
+    } catch {
+        return {}
+    }
+}
+
+export default {
+    getCityPolygon,
+    getCountryPolygon,
+    getRegionPolygon,
+    getSubregionPolygon
+}
