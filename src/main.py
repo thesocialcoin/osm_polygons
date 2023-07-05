973469cbc2940a5a48e29167a5e4d4be9522a2bf
Óscar Delgado <odelgado@citibeats.com>
2023-07-05 13:32:56 +0200

    Added endpoints and tweeked features from file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src import __version__
-from src.utils import get_features_from_file
+from src.utils import get_features_from_file, get_feature_from_nominatim
 from src.validators import validate_zoom_level
 
 
-app = FastAPI(
-    version=__version__
+app = FastAPI(version=__version__)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"],
+    allow_origins=["*"],
 )
 
 
@@ -20,3 +27,10 @@ def polygons(zoom_level: str, codes: str = ""):
     validate_zoom_level(zoom_level)
 
     return get_features_from_file(zoom_level, codes)
+
+
+@app.post("/add_polygon")
+def add_polygon(zoom_level: str, code: str):
+    validate_zoom_level(zoom_level)
+
+    return get_feature_from_nominatim(zoom_level, code)
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,29 @@
 import json
 
+from fastapi import HTTPException
 
-def get_features_from_file(file: str, codes: str):
-    with open(f"geojsons/{file}.json", "r") as file:
+
+def get_features_from_file(zoom_level: str, codes: str):
+    with open(f"geojsons/{zoom_level}.json", "r") as file:
         all_features = json.load(file)
 
     features = {}
     for code in set(codes.split(",")):
-        try:
-            feature = all_features[code]
-            features[code] = feature
-        except KeyError:
-            continue
+        if zoom_level == "country":
+            try:
+                feature = all_features[code.upper()]
+                features[code.upper()] = feature
+            except KeyError:
+                continue
+        else:
+            try:
+                for key, value in all_features[code.upper()].items():
+                    features[key] = value
+            except KeyError:
+                continue
 
     return features
+
+
+def get_feature_from_nominatim(zoom_level: str, code: str):
+    raise HTTPException(status_code=418)
