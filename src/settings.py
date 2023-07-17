60d75f25219cb08821ea20eae17350a2a6ed3ed0
Oscar D <74262517+oscar-delgado@users.noreply.github.com>
2023-07-17 10:51:57 +0200

    💽 Fix MongoDB URL and Sentry environment (#3)

--- src/settings.py
+++ src/settings.py
@@ -4,9 +4,13 @@ from pydantic import BaseSettings
 
 import sentry_sdk
 
+from src import __version__
+
 
 class Settings(BaseSettings):
+    mongodb_url: str = "db"
     nominatim_endpoint: str = "http://nominatim.ctb.internal/details"
+    sentry_environment: str = "local"
     sentry_traces_sample_rate: float = 0
 
 
@@ -15,12 +19,14 @@ settings = Settings()
 
 sentry_sdk.init(
     dsn="https://d579829e2ab94aaf80e043fd0e82efba@issues.citibeats.com/29",
+    environment=settings.sentry_environment,
+    release=f"osm_polygons@{__version__}",
     traces_sample_rate=settings.sentry_traces_sample_rate
 )
 
 
 async def init_db():
-    client = AsyncIOMotorClient("mongodb://root:secret@db:27017")
+    client = AsyncIOMotorClient(f"mongodb://root:secret@{settings.mongodb_url}:27017")
 
     await init_beanie(
         database=client.polygons,
