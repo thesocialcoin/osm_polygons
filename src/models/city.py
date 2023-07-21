281c3922189082854d6dbe387b2f0938cb15aff9
Óscar Delgado <odelgado@citibeats.com>
2023-07-21 11:51:30 +0200

    🕵️ Indexed country_code in databases

--- src/models/city.py
+++ src/models/city.py
@@ -1,11 +1,11 @@
 from typing import Dict
 
-from beanie import Document
+from beanie import Document, Indexed
 
 
 class City(Document):
     id: str  # OSM ID
-    country_code: str  # ISO 3166 alpha-2
+    country_code: Indexed(str)  # ISO 3166 alpha-2
     type: str
     properties: Dict
     geometry: Dict
